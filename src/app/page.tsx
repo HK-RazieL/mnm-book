6375663b078615d0fdc57ce3bab6e9137b9a0890
Stanley-Codelevate <stanislav@codelevate.com>
2025-05-07 01:42:40 +0300

    lint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,5 @@
 "use client";
-import Image from "next/image";
-import { forwardRef } from "react";
-import HTMLFlipBook from "react-pageflip";
-import { useMediaQuery } from "usehooks-ts";
-
 
 export default function Home() {
-  return (
-    <div>
-     
-    </div>
-  );
+  return <div>Home</div>;
 }
